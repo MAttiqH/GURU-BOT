bfbac7c5c3ee6cb6e15266b34059ab93b973a85b
Attiq Ur Rehman <143476511+MAttiqH@users.noreply.github.com>
2024-03-01 14:35:19 +0500

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ global.owner = [
 //global.pairingNumber = "" //put your bot number here
 
 global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.prems = ['92475667330', '32460248586', '919398758484']
+global.allowed = ['923475667330', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,13 +50,13 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'TLP'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
+global.packname = 'TLP┃Team' 
+global.author = 'Attiq' 
+global.menuvid = 'https://i.imgur.com/de0ajoM.mp4'
+global.igfg = '▢ Follow\nhttps://whatsapp.com/channel/0029VaCbEyBLikgAnIHwi11w/130 \n' 
+global.dygp = 'https://whatsapp.com/channel/0029VaCbEyBLikgAnIHwi11w/130'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
 global.fgyt = 'https://youtube.com/@Asliguru'
 global.fgpyp = 'https://youtube.com/@Asliguru'
